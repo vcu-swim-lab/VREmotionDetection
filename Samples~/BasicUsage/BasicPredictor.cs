21cb86cfe1f8c7e71d32680d446f467688f9370a
Terens <37905314+TerensTare@users.noreply.github.com>
2025-11-18 11:24:36 -0500

    Improve example to show how to use confidence scores

--- Samples~/BasicUsage/BasicPredictor.cs
+++ Samples~/BasicUsage/BasicPredictor.cs
@@ -27,6 +27,15 @@ public class MyScript : MonoBehaviour
             // Confidence tells you how certain the model is about this emotion. You can ignore it most of the time.
             var (emo, confidence) = await predictor.Predict();
 
+            // Or, you can get a list of all confidences and process them yourself.
+            // NOTE: when calling `Predict` the model internally multiplies the confidences by some values,
+            // so using confidences (`PredictRaw`) is recommended when you have only natural or only acted data and the results
+            // will be slightly different from `Predict`.
+            var (nat, act) = await predictor.PredictRaw();
+            var natArr = nat.AsReadOnlyNativeArray(); // float[7] 0.0-1.0
+            var actArr = act.AsReadOnlyNativeArray(); // float[7] 0.0-1.0
+            // ... use the confidences here
+
             // use the emotion here, possible values are:
             // - Emotion.Anger
             // - Emotion.Disgust
